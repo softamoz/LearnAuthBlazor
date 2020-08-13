f41d7cdbc113f92241913391b03f5267c8b59223
seraj654 <seraj654@yahoo.com>
2020-08-13 16:52:19 +0430

    Add AuthorizationCore To Services

--- LearnAuthBlazor/Client/Program.cs
+++ LearnAuthBlazor/Client/Program.cs
@@ -19,6 +19,8 @@ namespace LearnAuthBlazor.Client
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+            builder.Services.AddAuthorizationCore();
+
             await builder.Build().RunAsync();
         }
     }
