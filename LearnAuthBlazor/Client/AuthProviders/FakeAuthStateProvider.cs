e5b799777ede01e2c67f8c179cf8c8f7a4ff1e1d
seraj654 <seraj654@yahoo.com>
2020-08-13 17:22:31 +0430

    Add FakeAuthStateProvider Class With Two Kind Of Users

---
+++ LearnAuthBlazor/Client/AuthProviders/FakeAuthStateProvider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace LearnAuthBlazor.Client.AuthProviders
+{
+    public class FakeAuthStateProvider : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            await Task.Delay(2000);
+
+
+            var anonymous = new ClaimsIdentity();   // Anonymous User (UnAuthorized)
+
+
+            var user = new ClaimsIdentity(new List<Claim>()     //Authenticated User
+            {
+                new Claim(ClaimTypes.Name,"John Doe"),
+                new Claim(ClaimTypes.Role,"Admin")
+
+            }, "Test And Fake");
+
+
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(user)));
+        }
+    }
+}
