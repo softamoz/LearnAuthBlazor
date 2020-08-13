114837d1bac0a8764944ff91b7629d2d443325b6
seraj654 <seraj654@yahoo.com>
2020-08-13 17:33:06 +0430

    Add FakeAuthStateProvider To DI Service

--- LearnAuthBlazor/Client/Program.cs
+++ LearnAuthBlazor/Client/Program.cs
@@ -3,6 +3,8 @@ using System.Net.Http;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Text;
+using LearnAuthBlazor.Client.AuthProviders;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -20,6 +22,8 @@ namespace LearnAuthBlazor.Client
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             builder.Services.AddAuthorizationCore();
+
+            builder.Services.AddScoped<AuthenticationStateProvider, FakeAuthStateProvider>();
 
             await builder.Build().RunAsync();
         }
