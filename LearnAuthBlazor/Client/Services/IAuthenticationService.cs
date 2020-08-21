4776a5208957a92b40e1a8f7a1841fe94a55f53d
seraj654 <seraj654@yahoo.com>
2020-08-21 15:41:31 +0430

    Add IAuthenticationService

---
+++ LearnAuthBlazor/Client/Services/IAuthenticationService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using LearnAuthBlazor.Shared.DTOs;
+
+namespace LearnAuthBlazor.Client.Services
+{
+    public interface IAuthenticationService
+    {
+        Task<RegistrationResponseDto> RegisterUser(UserForRegistrationDto userForRegistration);
+    }
+}
