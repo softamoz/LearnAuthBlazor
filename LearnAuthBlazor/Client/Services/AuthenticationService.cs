63f38d0b209a52742180d3eaafae5c1ec57d1d7f
seraj654 <seraj654@yahoo.com>
2020-08-21 16:18:35 +0430

    Add AuthenticationService

---
+++ LearnAuthBlazor/Client/Services/AuthenticationService.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using LearnAuthBlazor.Shared.DTOs;
+
+namespace LearnAuthBlazor.Client.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly HttpClient _client;
+
+        public AuthenticationService(HttpClient client)
+        {
+            _client = client;
+        }
+
+
+        public async Task<RegistrationResponseDto> RegisterUser(UserForRegistrationDto userForRegistration)
+        {
+            var content = JsonSerializer.Serialize(userForRegistration);
+            var bodyContent = new StringContent(content, Encoding.UTF8, "application/json");
+
+            var registrationResult = await _client.PostAsync("https://localhost:5011/api/accounts/registration", bodyContent);
+            var registrationContent = await registrationResult.Content.ReadAsStringAsync();
+
+            if (!registrationResult.IsSuccessStatusCode)
+            {
+                var result = JsonSerializer.Deserialize<RegistrationResponseDto>(registrationContent, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                return result;
+            }
+
+            return new RegistrationResponseDto { IsSuccessfulRegistration = true };
+        }
+    }
+}
